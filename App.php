d350abbc736f14cb98fdecda644584a01e715abc
Mariia Paraketsova <paraketsova@gmail.com>
2021-04-24 21:05:15 +0200

    Add price

--- App.php
+++ App.php
@@ -37,23 +37,24 @@ class App {
 
   /*** List all products from category ***/
   public static function viewData($array){
-
     $table = "<table class='table'>
     <tr>
       <th>Title</th>
       <th>Picture</th>
       <th>Description</th>
+      <th>Price</th>
     </tr>";
     //$categoryDone = $_GET['category'] ?? "Choose a category";
     $categoryDone = "women clothing";
     foreach ($array as $product) {
       if ($product['category'] === $categoryDone) {
         $url = $product['image'];
-        $img = "<img src='$url' class='img-fluid m-2' style='width: 70%;'>";
+        $img = "<img src='$url' class='img-fluid m-2' style='width: 100%;'>";
         $table .= "<tr>
           <td> $product[title] </td>
           <td> $img </td>
           <td> $product[description] </td>
+          <td> $product[price] SEK</td>
         </tr>";
       }
     }
