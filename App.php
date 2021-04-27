931b0f0de31bbf987176df58ffd1beb078ccedb8
Mariia Paraketsova <paraketsova@gmail.com>
2021-04-27 19:25:16 +0200

    Update card text - overflow hidden

--- App.php
+++ App.php
@@ -1,76 +1,84 @@
 <?php
 class App {
   public static $endpoint = "https://webacademy.se/fakestore/";
+
   /**
    * The Main method
    */
-  public static function main(){
+  public static function main() {
     try {
-      $array =  self::getData();
-      self::viewCategory($array);
-      self::viewData($array);
+      $array = self::getData();
+      self::viewCategories();
+      self::viewProducts($array);
     } catch (Exception $error) {
       echo $error->getMessage();
     }
   }
+
   /**
    * Gets data from endpoint
    */
-  public static function getData(){
+  public static function getData() {
     $json = @file_get_contents(self::$endpoint);
     if(!$json) {
       throw new Exception("Could not access " . self::$endpoint);
     }
     return json_decode($json, true);
   }
+
   /**
    * List the categories
    */
-  public static function viewCategory(){
+  public static function viewCategories() {
     $categories = array('women clothing', 'men clothing', 'jewelery');
 
     echo "<div class='col-2'>
-    <h4>Products categories:</h4>";
+      <h4>Products categories:</h4>";
     foreach ($categories as $category) {
-      echo "<a class='list-group-item list-group-item-action' href='?category=$category' class='link'>" . ucfirst($category) . "</a>";
+      echo "<a class='list-group-item list-group-item-action list-group-item-dark' href='?category=$category' id='link'>" . ucfirst($category) . "</a>";
     }
     echo "</div>";
   }
+
   /**
    * List all products from category
    */
-  public static function viewData($array){
+  public static function viewProducts($array) {
     $categoryDone = $_GET['category'] ?? null;
 
-    if(isset($categoryDone)) {
+    if (isset($categoryDone)) {
       $div = "<div class='row'>";
         foreach ($array as $product) {
           if ($product['category'] === $categoryDone) {
             $url = $product['image'];
-            $altText = "img of $product[title]";
-            $div .="
+            $altText = "Image for {$product['title']}";
+            $div .= "
               <div class='col-sm-3'>
                 <div class='card' id='cardB'>
-                  <img src='$url' class='card-img-top' alt='$altText'>
+                  <img src='{$url}' class='card-img-top' alt='{$altText}'>
                   <div class='card-body'>
-                    <h5 class='card-title' class='fw-bold'> $product[title] </h5>
-                    <p class='card-text' id='descript'> $product[description] </p>
+                    <h5 class='card-title' class='fw-bold'> {$product['title']} </h5>
+                    <p class='card-text'> {$product['description']} </p>
                   </div>
-                  <div class='card-footer'> <span class='fw-bold'>$product[price]</span> SEK</div>
+                  <div class='card-footer'>
+                    <span class='fw-bold'> {$product['price']} </span> SEK
+                  </div>
                 </div>
               </div>";
           }
         }
-      echo "<div class='col-9'>
-              <h4>" . strtoupper($categoryDone) . "</h4>
-              <h5>Products list:</h5>
-              $div
-            </div>";
+      echo "
+        <div class='col-9'>
+          <h4>" . strtoupper($categoryDone) . "</h4>
+          <h5>Products list:</h5>
+          {$div}
+        </div>";
     } else {
-      echo "<div class='col-4'>
-              <h4>Welcome to our store!</h4>
-              <h6> ← Choose a category from the list</h6>
-            </div>";
+      echo "
+        <div class='col-4'>
+          <h4>Welcome to our store!</h4>
+          <h6> ← Choose a category from the list</h6>
+        </div>";
     }
   }
 }
--- index.php
+++ index.php
@@ -6,15 +6,48 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="styles.css" rel="stylesheet">
+  <link href="./styles.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <title>FakeStoreApi</title>
 </head>
 ​
 <body class="container">
-  <div class="header shadow-sm p-3 mt-3 mb-3 rownded"><h2>FAKESTORE</h2></div>
+  <div class="header shadow-sm p-3 mt-3 mb-3 rounded">
+    <h1 id='title'>FAKESTORE</h1>
+  </div>
   <div class="row">
     <?php App::main(); ?>
+  </div>
+
+  <div class="mt-5 pt-5 pb-5 footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-xs-12 about-company">
+          <h2 id="titleFooter">FAKESTORE</h2>
+          <p class="pr-5 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis </p>
+          <p><a href="#"><i class="fa fa-facebook-square mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
+        </div>
+        <div class="col-lg-3 col-xs-12 links">
+          <h4 class="mt-lg-0 mt-sm-3">Links</h4>
+            <ul class="m-0 p-0">
+              <li><a href="#">Lorem ipsum</a></li>
+              <li><a href="#">Nam mauris velit</a></li>
+              <li><a href="#">Etiam vitae mauris</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-4 col-xs-12 location">
+          <h4 class="mt-lg-0 mt-sm-4">Location</h4>
+          <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
+          <p class="mb-0"><i class="fa fa-phone mr-3"></i>(541) 754-3010</p>
+          <p><i class="fa fa-envelope-o mr-3"></i>info@hsdf.com</p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col" id="copyright">
+          <p class=""><small class="text-white-50">© 2021. All Rights Reserved.</small></p>
+        </div>
+      </div>
+    </div>
   </div>
 </body>
 ​
